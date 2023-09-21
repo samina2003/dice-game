19d1777497dac3e27c6722e782dc088b31193199
SAMINA KHATOON <93727445+samina2003@users.noreply.github.com>
2023-09-21 22:19:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+//1st image
+
+var randomNumber1 = Math.floor(Math.random()*6)+1 ;
+
+var randomDiceImage1 = "images/Dice" + randomNumber1 + ".png";//images/dice1.png-images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1 );;
+
+
+
+
+//2nd image
+
+var randomNumber2 = Math.floor(Math.random()*6)+1 ;
+
+var randomDiceImage2 = "images/Dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+
+
+///CODE IN 1 LINE 
+///document.querySelectorAll("img")[1].setAttribute("src","images/Dice"+ (Math.floor(Math.random()*6) +1) + ".png");
+
+
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML=("Player 1 Wins!");
+  }
+ else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML=("Player 2 Wins!");
+  }
+  else {
+    document.querySelector("h1").innerHTML=("Draw!");
+  }
